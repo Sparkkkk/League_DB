cf838e8d31a39d79388fafe1f0f01b62a41d63b4
na-wu <nwu1018@gmail.com>
2019-06-18 22:17:39 -0700

    connect DB, renders view

--- leagueDB_app/views.py
+++ leagueDB_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the League index.")
+    return HttpResponse('Reached LeagueDB_app')
 
+def hello(request):
+    return render(request, 'leagueDB_app/index.html')
 # Create your views here.
--- league_DB/settings.py
+++ league_DB/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leagueDB_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'league_DB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ghwjlzvb',
+        'USER': 'ghwjlzvb',
+        'PASSWORD': 'zyLL4t30ygjtCs6m5BeVqtOYYX2E4u2L',
+        'HOST': 'raja.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
