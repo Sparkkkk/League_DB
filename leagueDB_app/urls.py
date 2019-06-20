f1c892dd30e2d6e5ed6b9c9438ec02021fe47394
Wei Sun <sparksun37@gmail.com>
2019-06-19 19:13:04 -0700

    implemented 4 page in view without adding acutal SQL, and added simple selekton html for each pages

--- leagueDB_app/urls.py
+++ leagueDB_app/urls.py
@@ -10,5 +10,14 @@ urlpatterns = [
     # leaguedb_app/hello
     path('page1', views.hello, name='page1'),
     # leaguedb_app/hello/
-    path(r'page1/(a-z)*', views.page2, name='page2'),
+    # path(r'page1/(a-z)*', views.page2, name='page2'),
+
+    # testing page for page2
+    path('page2', views.page2, name='page2'),
+
+    # testing page for page2
+    path('page3', views.page3, name='page3'),
+
+    # testing page for page2
+    path('page4', views.page4, name='page4'),
 ]
--- leagueDB_app/views.py
+++ leagueDB_app/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import LeagueDBModel
 
+
 def index(request):
     return HttpResponse('Reached LeagueDB_app')
+
 
 def hello(request):
     leagueList = LeagueDBModel.objects.raw('SELECT id, col from "leagueDB_app_leaguedbmodel"')
@@ -15,7 +17,43 @@ def hello(request):
     return HttpResponse(template.render(context, request))
     # return render(request, 'leagueDB_app/index.html')
 
-def page2(request): 
-    template = loader.get_template('leagueDB_app/index.html')
-    return HttpResponse(template.render(request))
+# Those None need to be replaced by SQL
+def page2(request):
+    # for page 2 username is the parameter passed from URL
+    username = request.content_params.get('username')
+    top5_champions = None
+    matches = None
+    context = {
+        'top5': top5_champions,
+        'matches': matches
+    }
+    template = loader.get_template('leagueDB_app/page2.html')
+    return HttpResponse(template.render(context, request))
+
+
+def page3(request):
+
+    champions_name = None
+    winrate_with_champion_name = None
+    context = {
+        'champ_name': champions_name,
+        'winrate': winrate_with_champion_name
+    }
+    template = loader.get_template('leagueDB_app/page3.html')
+    return HttpResponse(template.render(context, request))
+
+
+def page4(request):
+    # for page 4, champion name is the parameter passed from URL
+    champion_name = request.content_params.get('champion_name')
+    top5_player = None
+    recommond_items = None
+    context = {
+        'top5': top5_player,
+        'recom': recommond_items
+    }
+
+    template = loader.get_template('leagueDB_app/page4.html')
+    return HttpResponse(template.render(context, request))
+
 # Create your views here.
